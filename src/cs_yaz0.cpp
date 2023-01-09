d53abb3a8f58d00e99be09209cd60c88529019b4
ArchLeaders <archparliament@gmail.com>
2023-01-08 17:34:38 -0800

    use &front() for u8* cast

--- src/cs_yaz0.cpp
+++ src/cs_yaz0.cpp
@@ -1,12 +1,15 @@
 #include "include/cs_yaz0.h"
 
+#include <fstream>
+#include <iostream>
+
 using namespace oead;
 
 u8* Compress(const u8* src, int src_len, int& dst_len, u32 data_alignment, int level) {
     auto data = tcb::span<const u8>(src, src_len);
     auto dst_data = yaz0::Compress(data, data_alignment, level);
     dst_len = dst_data.size();
-    return dst_data.data();
+    return &dst_data.front();
 }
 
 void Decompress(const u8* src, int src_len, u8* dst, int dst_len) {
