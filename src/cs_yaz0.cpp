1d433a7e2e3a3657b40d3d07b59bb2f00b006860
ArchLeaders <archparliament@gmail.com>
2023-01-09 17:44:46 -0800

    Impl copy-free compression wrapper

--- src/cs_yaz0.cpp
+++ src/cs_yaz0.cpp
@@ -1,19 +1,21 @@
 #include "include/cs_yaz0.h"
 
-#include <fstream>
-#include <iostream>
-
 using namespace oead;
 
-u8* Compress(const u8* src, int src_len, int& dst_len, u32 data_alignment, int level) {
-    auto data = tcb::span<const u8>(src, src_len);
-    auto dst_data = yaz0::Compress(data, data_alignment, level);
-    dst_len = dst_data.size();
-    return &dst_data.front();
+void Compress(const u8* src, u32 src_len, void** dst_handle, u8** dst, u32* dst_len, u32 data_alignment, int level) {
+    auto* result = new auto{yaz0::Compress({src, src_len}, data_alignment, level)};
+    *dst_handle = result;
+    *dst = result->data();
+    *dst_len = result->size();
 }
 
 void Decompress(const u8* src, int src_len, u8* dst, int dst_len) {
     auto src_span = tcb::span<const u8>(src, src_len);
     auto dst_span = tcb::span<u8>(dst, dst_len);
     yaz0::Decompress(src_span, dst_span);
+}
+
+bool FreeResource(void* vector_ptr) {
+    delete static_cast<std::vector<u8>*>(vector_ptr);
+    return true;
 }
--- src/include/cs_yaz0.h
+++ src/include/cs_yaz0.h
@@ -13,8 +13,9 @@ extern "C" {
 #define CS_YAZ0 __declspec(dllimport)
 #endif
 
-u8* CS_YAZ0 Compress(const u8* src, int src_len, int& dst_len, u32 data_alignment, int level);
+void CS_YAZ0 Compress(const u8* src, u32 src_len, void** dst_handle, u8** dst, u32* dst_len, u32 data_alignment, int level);
 void CS_YAZ0 Decompress(const u8* src, int src_len, u8* dst, int dst_len);
+bool CS_YAZ0 FreeResource(void* vector_ptr);
 
 #ifdef __cplusplus
 }
