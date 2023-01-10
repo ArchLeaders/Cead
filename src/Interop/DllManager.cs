b1445994a5462920b489e517e29771fcca3ebd81
ArchLeaders <archparliament@gmail.com>
2023-01-10 00:42:33 -0800

    Impl DLL manager class

---
+++ src/Interop/DllManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Cead.Interop
+{
+    public static class DllManager
+    {
+        private static readonly string[] Libs = { "Cead.dll" };
+        private static bool _isLoaded;
+
+        [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static extern IntPtr SetDllDirectory(string lpFileName);
+
+        internal static void LoadDlls()
+        {
+            if (_isLoaded) {
+                return;
+            }
+
+            string path = Path.Combine(Path.GetTempPath(), $"Cead-{typeof(Yaz0).Assembly.GetName().Version}");
+
+            foreach (var lib in Libs) {
+                string dll = Path.Combine(path, lib);
+                // Always copy in debug mode
+#if DEBUG
+                Directory.CreateDirectory(path);
+                using Stream stream = Assembly.GetCallingAssembly().GetManifestResourceStream($"Yaz0Library.Lib.Cead.dll")!;
+                using FileStream fs = File.Create(dll);
+                stream.CopyTo(fs);
+#else
+                if (!File.Exists(dll)) {
+                    Directory.CreateDirectory(path);
+                    using Stream stream = Assembly.GetCallingAssembly().GetManifestResourceStream($"Yaz0Library.Lib.Cead.dll");
+                    using FileStream fs = File.Create(dll);
+                    stream.CopyTo(fs);
+                }
+#endif
+            }
+
+            SetDllDirectory(path);
+            _isLoaded = true;
+        }
+    }
+}
