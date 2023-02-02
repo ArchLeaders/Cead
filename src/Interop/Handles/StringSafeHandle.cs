1935ffe992c71e68bd709f5d31ea0f260b13d187
ArchLeaders <archparliament@gmail.com>
2023-02-01 19:24:43 -0800

    Impl StringSafeHandle

---
+++ src/Interop/Handles/StringSafeHandle.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System.Runtime.InteropServices;
+
+namespace Cead.Interop.Handles;
+
+public partial class StringSafeHandle : SafeHandleZeroOrMinusOneIsInvalid
+{
+    [LibraryImport("Cead.lib")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static partial bool FreeString(IntPtr string_ptr);
+
+    public StringSafeHandle() : base(true) { }
+    protected override bool ReleaseHandle()
+    {
+        return FreeString(handle);
+    }
+}
