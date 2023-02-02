3d7bf33488ff952264a10a06cf7c555cacbee24e
ArchLeaders <archparliament@gmail.com>
2023-02-01 19:04:13 -0800

    Move VoidSafeHandle (VectorSafeHandle)

---
+++ src/Interop/Handles/VectorSafeHandle.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+namespace Cead.Interop
+{
+    public class VectorSafeHandle : SafeHandleZeroOrMinusOneIsInvalid
+    {
+        public VectorSafeHandle() : base(true) { }
+        protected override bool ReleaseHandle()
+        {
+            return Yaz0.FreeVector(handle);
+        }
+    }
+}
