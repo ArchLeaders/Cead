0394ad21b86f9908891becedd532c34f8ced86ce
ArchLeaders <archparliament@gmail.com>
2023-02-02 20:22:52 -0800

    Impl BYML object getters (C++)

--- native/src/cs_byml.cpp
+++ native/src/cs_byml.cpp
@@ -10,15 +10,69 @@ void FromText(const char* src, void** dst) {
     *dst = new auto{byml};
 }
 
-void ToBinary(Byml* byml, void** dst_handle, u8** dst, u32* dst_len, bool big_endian, int version) {
+void ToBinary(Byml* byml, void** vector_handle, u8** dst, u32* dst_len, bool big_endian, int version) {
     auto* result = new auto{byml->ToBinary(big_endian, version)};
-    *dst_handle = result;
+    *vector_handle = result;
     *dst = result->data();
     *dst_len = result->size();
 }
 
-void ToText(Byml* byml, void** dst_handle, const char** dst) {
+void ToText(Byml* byml, void** string_handle, const char** dst) {
     auto str = new auto{byml->ToText()};
-    *dst_handle = str;
+    *string_handle = str;
     *dst = str->c_str();
+}
+
+Byml::Type GetType(Byml* byml) {
+    return byml->GetType();
+}
+
+void GetHash(Byml* byml, void** dst) {
+    *dst = new auto{byml->GetHash()};
+}
+
+void GetArray(Byml* byml, void** vector_handle, Byml** dst, u32* dst_size) {
+    auto* result = new auto{byml->GetArray()};
+    *dst = result->data();
+    *dst_size = result->size();
+}
+
+void GetString(Byml* byml, void** string_handle, const char** dst) {
+    auto str = new auto{byml->ToText()};
+    *string_handle = str;
+    *dst = str->c_str();
+}
+
+void GetBinary(Byml* byml, void** vector_handle, u8** dst, u32* dst_size) {
+    auto* result = new auto{byml->GetBinary()};
+    *dst = result->data();
+    *dst_size = result->size();
+}
+
+bool GetBool(Byml* byml) {
+    return byml->GetBool();
+}
+
+s32 GetInt(Byml* byml) {
+    return byml->GetInt();
+}
+
+u32 GetUInt(Byml* byml) {
+    return byml->GetUInt();
+}
+
+f32 GetFloat(Byml* byml) {
+    return byml->GetFloat();
+}
+
+s64 GetInt64(Byml* byml) {
+    return byml->GetInt64();
+}
+
+u64 GetUInt64(Byml* byml) {
+    return byml->GetUInt64();
+}
+
+f64 GetDouble(Byml* byml) {
+    return byml->GetDouble();
 }
--- native/src/include/cs_byml.h
+++ native/src/include/cs_byml.h
@@ -11,8 +11,22 @@ using namespace oead;
 
 void CS_BYML FromBinary(const u8* src, u32 src_len, void** dst);
 void CS_BYML FromText(const char* src, void** dst);
-void CS_BYML ToBinary(Byml* byml, void** dst_handle, u8** dst, u32* dst_len, bool big_endian, int version);
-void CS_BYML ToText(Byml* byml, void** dst_handle, const char** dst);
+void CS_BYML ToBinary(Byml* byml, void** vector_handle, u8** dst, u32* dst_len, bool big_endian, int version);
+void CS_BYML ToText(Byml* byml, void** string_handle, const char** dst);
+
+Byml::Type CS_BYML GetType(Byml* byml);
+
+void CS_BYML GetHash(Byml* byml, void** dst);
+void CS_BYML GetArray(Byml* byml, void** vector_handle, Byml** dst, u32* dst_size);
+void CS_BYML GetString(Byml* byml, void** string_handle, const char** dst);
+void CS_BYML GetBinary(Byml* byml, void** vector_handle, u8** dst, u32* dst_size);
+bool CS_BYML GetBool(Byml* byml);
+s32 CS_BYML GetInt(Byml* byml);
+u32 CS_BYML GetUInt(Byml* byml);
+f32 CS_BYML GetFloat(Byml* byml);
+s64 CS_BYML GetInt64(Byml* byml);
+u64 CS_BYML GetUInt64(Byml* byml);
+f64 CS_BYML GetDouble(Byml* byml);
 
 #ifdef __cplusplus
 }
