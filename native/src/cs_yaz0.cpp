146e2ab39ac04efba739a1a7cf8dc469c19e7115
ArchLeaders <archparliament@gmail.com>
2023-03-02 20:07:12 -0800

    Cleanup native Yaz0 impl

--- native/src/cs_yaz0.cpp
+++ native/src/cs_yaz0.cpp
@@ -1,7 +1,5 @@
 #include "include/cs_yaz0.h"
 
-using namespace oead;
-
 void Compress(const u8* src, u32 src_len, void** dst_handle, u8** dst, u32* dst_len, u32 data_alignment, int level) {
     auto* result = new auto{yaz0::Compress({src, src_len}, data_alignment, level)};
     *dst_handle = result;
