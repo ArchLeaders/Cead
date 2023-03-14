129ab8ffa616d2d666c22c363f7961a8c80aca0c
ArchLeaders <archparliament@gmail.com>
2023-03-13 21:04:41 -0700

    Cleanup Yaz0 impl

--- src/Yaz0.cs
+++ src/Yaz0.cs
@@ -1,5 +1,4 @@
 ﻿using Cead.Interop;
-using Cead.Interop.Handles;
 using System.Buffers.Binary;
 using System.Runtime.InteropServices;
 
@@ -7,28 +6,21 @@ namespace Cead;
 
 public static unsafe partial class Yaz0
 {
-    [LibraryImport("Cead.lib")]
-    internal static unsafe partial void Compress(byte* src, int src_len, out VectorSafeHandle dst_handle, out byte* dst, out int dst_len, uint data_alignment, int level);
+    [LibraryImport("Cead.lib")] internal static partial void Compress(byte* src, int src_len, out PtrHandle dst_handle, out byte* dst, out int dst_len, uint data_alignment, int level);
+    [LibraryImport("Cead.lib")] internal static partial void Decompress(byte* src, int src_len, byte* dst, int dst_len);
 
-    [LibraryImport("Cead.lib")]
-    internal static unsafe partial void Decompress(byte* src, int src_len, byte* dst, int dst_len);
-
-    public static unsafe Span<byte> Compress(string file, out VectorSafeHandle handle, int level = 7) => Compress(File.ReadAllBytes(file), out handle, level);
-    public static unsafe Span<byte> Compress(ReadOnlySpan<byte> src, out VectorSafeHandle handle, int level = 7)
+    public static Span<byte> Compress(string file, out PtrHandle handle, int level = 7) => Compress(File.ReadAllBytes(file), out handle, level);
+    public static Span<byte> Compress(ReadOnlySpan<byte> src, out PtrHandle handle, int level = 7)
     {
-        DllManager.Load();
-
         fixed (byte* srcPtr = src) {
             Compress(srcPtr, src.Length, out handle, out byte* dst, out int dstLen, 0, level);
             return new(dst, dstLen);
         }
     }
 
-    public static unsafe Span<byte> Decompress(string file) => Decompress(File.ReadAllBytes(file));
-    public static unsafe Span<byte> Decompress(ReadOnlySpan<byte> src)
+    public static Span<byte> Decompress(string file) => Decompress(File.ReadAllBytes(file));
+    public static Span<byte> Decompress(ReadOnlySpan<byte> src)
     {
-        DllManager.Load();
-
         Span<byte> dst = new byte[BinaryPrimitives.ReadUInt32BigEndian(src[0x04..0x08])];
 
         fixed (byte* src_ptr = src) {
