c8e98a89d2d50d6840849793518e7dcec145a368
ArchLeaders <archparliament@gmail.com>
2023-03-14 01:28:38 -0700

    Impl SARC IO functions (c++)

--- native/src/cs_sarc.cpp
+++ native/src/cs_sarc.cpp
@@ -1,1 +1,70 @@
 #include "include/cs_sarc.h"
+
+Sarc* SarcFromBinary(u8* src, u32 src_len) {
+    return new auto{Sarc({src, src_len})};
+}
+
+void SarcToBinary(SarcWriter* writer, void** handle, u8** dst, u32* dst_len) {
+    auto result = new auto{writer->Write()};
+    *handle = result;
+    *dst = result->second.data();
+    *dst_len = result->second.size();
+}
+
+u32 GetNumFiles(Sarc* sarc) {
+    return sarc->GetNumFiles();
+}
+
+u32 GetFileMapCount(SarcWriter* writer) {
+    return writer->m_files.size();
+}
+
+util::Endianness GetEndianess(Sarc* sarc) {
+    return sarc->GetEndianness();
+}
+
+void SetEndianess(SarcWriter* writer, util::Endianness endianess) {
+    writer->SetEndianness(endianess);
+}
+
+bool GetFile(Sarc* sarc, char* name, const u8** dst, u32* dst_len) {
+    auto file = sarc->GetFile(name);
+    if (file.has_value()) {
+        *dst = file.emplace().data.data();
+        *dst_len = file.emplace().data.size();
+        return true;
+    }
+
+    return false;
+}
+
+SarcWriter* NewSarcWriter(util::Endianness endian, SarcWriter::Mode mode) {
+    return new auto{SarcWriter(endian, mode)};
+}
+
+SarcWriter* GetSarcWriter(Sarc* sarc) {
+    return new auto{SarcWriter::FromSarc(*sarc)};
+}
+
+void SetWriterMode(SarcWriter* writer, SarcWriter::Mode mode) {
+    writer->SetMode(mode);
+}
+
+bool SarcWriterGet(SarcWriter* writer, char* name, u8** dst, u32* dst_len) {
+    auto vec = writer->m_files.at(name);
+    *dst = vec.data();
+    *dst_len = vec.size();
+    return true;
+}
+
+void AddSarcFile(SarcWriter* writer, char* name, u8* src, u32 src_len) {
+    writer->m_files.insert_or_assign(name, std::vector<u8>(src, src + src_len));
+}
+
+void RemoveSarcFile(SarcWriter* writer, char* name) {
+    writer->m_files.erase(name);
+}
+
+void ClearSarcFiles(SarcWriter* writer) {
+    writer->m_files.clear();
+}
--- native/src/include/cs_sarc.h
+++ native/src/include/cs_sarc.h
@@ -6,4 +6,19 @@
 extern "C" {
 
 using namespace oead;
+
+CEAD Sarc* SarcFromBinary(u8* src, u32 src_len);
+CEAD void SarcToBinary(SarcWriter* writer, void** handle, u8** dst, u32* dst_len);
+CEAD u32 GetNumFiles(Sarc* sarc);
+CEAD u32 GetFileMapCount(SarcWriter* writer);
+CEAD util::Endianness GetEndianess(Sarc* sarc);
+CEAD void SetEndianess(SarcWriter* writer, util::Endianness endianess);
+CEAD bool GetFile(Sarc* sarc, char* name, const u8** dst, u32* dst_len);
+CEAD SarcWriter* NewSarcWriter(util::Endianness endian, SarcWriter::Mode mode);
+CEAD SarcWriter* GetSarcWriter(Sarc* sarc);
+CEAD void SetWriterMode(SarcWriter* writer, SarcWriter::Mode mode);
+CEAD bool SarcWriterGet(SarcWriter* writer, char* name, u8** dst, u32* dst_len);
+CEAD void AddSarcFile(SarcWriter* writer, char* name, u8* src, u32 src_len);
+CEAD void RemoveSarcFile(SarcWriter* writer, char* name);
+CEAD void ClearSarcFiles(SarcWriter* writer);
 }
