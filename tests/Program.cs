e42fef9972efb22f5fb51209a36ed89b0c3bdb00
ArchLeaders <80713508+ArchLeaders@users.noreply.github.com>
2023-05-11 11:34:42 -0700

    Fix test program

--- tests/Program.cs
+++ tests/Program.cs
@@ -1,12 +1,12 @@
-﻿using Cead;
-using Cead.Interop.Handles;
+using Cead;
+using Cead.Handles;
 using System.Diagnostics;
 
 Stopwatch watch = Stopwatch.StartNew();
 
 byte[] data = File.ReadAllBytes("D:\\Bin\\BymlLibrary\\ActorInfo.product.byml");
 Byml byml = new(data);
-File.WriteAllText("D:\\Bin\\BymlLibrary\\ActorInfo.product.yml", byml.ToText(out StringSafeHandle _));
+File.WriteAllText("D:\\Bin\\BymlLibrary\\ActorInfo.product.yml", byml.ToText(out StringHandle _));
 
 watch.Stop();
 Console.WriteLine(watch.ElapsedMilliseconds);
@@ -14,7 +14,7 @@ watch.Restart();
 
 string text = File.ReadAllText("D:\\Bin\\BymlLibrary\\ActorInfo.product.yml");
 byml = new(text);
-File.WriteAllBytes("D:\\Bin\\BymlLibrary\\WRITE-ActorInfo.product.byml", byml.ToBinary(out VectorSafeHandle _, true).ToArray());
+File.WriteAllBytes("D:\\Bin\\BymlLibrary\\WRITE-ActorInfo.product.byml", byml.ToBinary(out DataHandle _, true).ToArray());
 
 watch.Stop();
 Console.WriteLine(watch.ElapsedMilliseconds);
