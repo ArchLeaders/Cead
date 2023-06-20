11022852c72e396eeee55eadb99ec37882732833
ArchLeaders <archparliament@gmail.com>
2023-06-19 18:19:01 -0700

    Update program cs

--- tests/Program.cs
+++ tests/Program.cs
@@ -1,21 +1,10 @@
 ﻿using Cead;
-using Cead.Interop.Handles;
-using System.Diagnostics;
+using Cead.Interop;
 
-Stopwatch watch = Stopwatch.StartNew();
+DllManager.LoadCead();
 
-byte[] data = File.ReadAllBytes("D:\\Bin\\BymlLibrary\\ActorInfo.product.byml");
-Byml byml = new(data);
-File.WriteAllText("D:\\Bin\\BymlLibrary\\ActorInfo.product.yml", byml.ToText(out StringSafeHandle _));
+Span<byte> data = File.ReadAllBytes(@"D:\Bin\AampLibrary\AirWall.bxml");
+ParameterIO pio = ParameterIO.FromBinary(data);
 
-watch.Stop();
-Console.WriteLine(watch.ElapsedMilliseconds);
-watch.Restart();
-
-string text = File.ReadAllText("D:\\Bin\\BymlLibrary\\ActorInfo.product.yml");
-byml = new(text);
-File.WriteAllBytes("D:\\Bin\\BymlLibrary\\WRITE-ActorInfo.product.byml", byml.ToBinary(out VectorSafeHandle _, true).ToArray());
-
-watch.Stop();
-Console.WriteLine(watch.ElapsedMilliseconds);
-watch.Restart();
+using FileStream fs = File.Create(@"D:\Bin\AampLibrary\AirWall.yml");
+fs.Write(pio.ToText().AsSpan());
